2897241991b2c3b83a06223ba66bd4e4226396ad
Kuldeep Singh <2019083@iiitdmj.ac.in>
2021-10-04 15:04:25 +0530

    server

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,5 @@
-const router = require("express").Router;
+const express = require("express");
+const router = express.Router();
 let User = require("../models/user.model");
 
 router.route("/").get((req, res) => {
