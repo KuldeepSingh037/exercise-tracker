8a2b572dc608c7ea117af829663e08a501275616
Kuldeep Singh <2019083@iiitdmj.ac.in>
2021-10-05 21:48:30 +0530

    database connection error

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-// new version of express already contains the bodyParser
-// const bodyParser = require("body-parser");
 
 require("dotenv").config();
 const app = express();
 const port = process.env.port || 5000;
 
-app.use(cors);
 app.use(express.json());
+app.use(cors);
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true });
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established securely.");
